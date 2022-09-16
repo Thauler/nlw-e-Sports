0579a00db9605a00faef6b97ce9a455a5a86d419
Thauler <thauler98@gmail.com>
2022-09-16 17:07:44 -0300

    refactor(utils): changes the field ads message

--- mobile/src/utils/games.ts
+++ mobile/src/utils/games.ts
@@ -2,37 +2,37 @@ export const GAMES = [
   {
     id: '1',
     name: 'League of Legends',
-    ads: '4',
+    ads: '4 anúncios',
     cover: require('../assets/games/game-1.png')
   },
   {
     id: '2',
     name: 'Dota II',
-    ads: '3',
+    ads: '3 anúncios',
     cover: require('../assets/games/game-2.png')
   },
   {
     id: '3',
     name: 'Counter-Strike',
-    ads: '2',
+    ads: '2 anúncios',
     cover: require('../assets/games/game-3.png')
   },
   {
     id: '4',
     name: 'Apex Legends',
-    ads: '3',
+    ads: '3 anúncios',
     cover: require('../assets/games/game-4.png')
   },
   {
     id: '5',
     name: 'Fortnite',
-    ads: '5',
+    ads: '5 anúncios',
     cover: require('../assets/games/game-5.png')
   },
   {
     id: '6',
     name: 'World of Warcraft',
-    ads: '2',
+    ads: '2 anúncios',
     cover: require('../assets/games/game-6.png')
   },
 ];
